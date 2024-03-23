efaee2b57231e8c7b2979edd962b2fccf2ac244f
rogerabucejojr <rogerhapay@gmail.com>
2024-03-23 15:35:01 +0800

    change dht11 to dht22

--- test-code/dht22.py
+++ test-code/dht22.py
@@ -2,7 +2,7 @@ import Adafruit_DHT
 import time
 
 # Set the sensor type: DHT11, DHT22, or AM2302
-SENSOR = Adafruit_DHT.DHT22
+SENSOR = Adafruit_DHT.DHT11
 
 # Set the GPIO pin where the sensor is connected
 PIN = 27
@@ -11,7 +11,7 @@ PIN = 27
 def read_dht_sensor():
     # Attempt to get a sensor reading
     humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
-    temperature = temperature * (9 / 5) + 32
+    # temperature = temperature * (9 / 5) + 32
     # Check if reading was successful
     if humidity is not None and temperature is not None:
         print(f"Temperature={temperature:.2f}°C, Humidity={humidity:.2f}%")
