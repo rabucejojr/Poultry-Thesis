b75620521d7840d4ed78f730fd90c355acd56f23
rogerabucejojr <rogerhapay@gmail.com>
2024-04-10 18:57:33 +0800

    edit

--- test-code/servo.py
+++ test-code/servo.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 26
-GPIO.setmode(GPIO.BCM)
+servoPIN = 37
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
