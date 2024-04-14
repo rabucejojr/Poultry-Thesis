1984eb3509579f253c1a24d06bbc93b383823dbe
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-04-14 15:28:02 +0800

    change code

--- test-code/measure_dist.py
+++ test-code/measure_dist.py
@@ -1,8 +1,47 @@
-from gpiozero import DistanceSensor
-from time import sleep
+import RPi.GPIO as GPIO
+import time
 
-sensor = DistanceSensor(5, 6) #GPIO5 and GPIO6
+# Set GPIO mode (BCM or BOARD)
+GPIO.setmode(GPIO.BCM)
 
-while True:
-    print('Distance to nearest object is', sensor.distance, 'm')
-    sleep(1)
+# Set GPIO pins for trigger and echo
+TRIG_PIN = 5
+ECHO_PIN = 6
+
+# Set up GPIO pins
+GPIO.setup(TRIG_PIN, GPIO.OUT)
+GPIO.setup(ECHO_PIN, GPIO.IN)
+
+def measure_distance():
+    # Ensure the trigger pin is low initially
+    GPIO.output(TRIG_PIN, False)
+    time.sleep(0.1)
+
+    # Send a 10us pulse to trigger
+    GPIO.output(TRIG_PIN, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG_PIN, False)
+
+    # Measure the pulse duration on the echo pin
+    while GPIO.input(ECHO_PIN) == 0:
+        pulse_start = time.time()
+
+    while GPIO.input(ECHO_PIN) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    # Speed of sound in air: 343 m/s or 34300 cm/s
+    # Distance = (time taken for pulse to return * speed of sound) / 2
+    distance = (pulse_duration * 34300) / 2
+
+    return distance
+
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = measure_distance()
+            print("Distance: %.1f cm" % dist)
+            time.sleep(1)
+    except KeyboardInterrupt:
+       
