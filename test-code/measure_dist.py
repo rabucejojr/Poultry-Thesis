9b4403d427f53c16b69876c3a70c14259683b76b
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-04-14 14:10:31 +0800

    gpio pins

--- test-code/measure_dist.py
+++ test-code/measure_dist.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(23, 24)
+sensor = DistanceSensor(5, 6)
 
 while True:
     print('Distance to nearest object is', sensor.distance, 'm')
