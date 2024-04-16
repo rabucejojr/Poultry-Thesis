a4502fad1afe599dccf251ecdeb0403fcd7901e0
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-04-16 21:21:42 +0800

    change board type

--- auto-feeder.py
+++ auto-feeder.py
@@ -13,15 +13,15 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 # Direction pin from controller
-DIR = 10
+DIR = 15
 # Step pin from controller
-STEP = 8
+STEP =14
 # 0/1 used to signify clockwise or counterclockwise.
 CW = 1
 CCW = 0
-servoPIN = 29 # pin sequence number 29 as per datasheet pinout
+servoPIN = 5 # pin sequence number 29 as per datasheet pinout
 # Setup pin layout on PI
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Establish Pins in software
