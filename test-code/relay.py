4610d79ab53bffbb8a2430d9e9401ad96bf66baa
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-04-17 08:44:41 +0800

    add relay code

---
+++ test-code/relay.py
@@ -0,0 +1,17 @@
+from gpiozero import OutputDevice
+from time import sleep
+
+pin1 = 17
+
+relay1 = OutputDevice(pin1,active_high=False, initial_value=False)
+try:
+    while True:
+        relay1.on()
+        sleep(2)
+        relay1.off()
+        sleep(2)
+except KeyboardInterrupt:
+    print("Exiting Program")
+
+finally:
+    relay1.close()
